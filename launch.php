c474b222d2c9d7754811b2d4045af9f330e36a6f
TEAMSOFTTG <tgteam351@gmail.com>
2016-05-04 21:43:21 +0430

    Update launch.php

--- launch.php
+++ launch.php
@@ -4,7 +4,7 @@ set_time_limit(0);
 
 require_once 'Elemental.php';
 
-define('BOT_TOKEN', 'Token');
+define('BOT_TOKEN', '224886858:AAHXceukVK93JMOQDCq61IBkbFCL9U60kGs');
 
 $bot = new PollBot(BOT_TOKEN, 'PollBotChat');
 $bot->runLongpoll();
